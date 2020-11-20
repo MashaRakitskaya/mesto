9b84a478bdd3a1424be54450bb5a947fb0064da0
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2020-11-20 10:27:48 +0100

    активность кнопки при неправельном введении

--- scripts/validate.js
+++ scripts/validate.js
@@ -26,11 +26,30 @@ const checkInputValidity = (formElement, inputElement) => {
     }
 };
 
+
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+};
+
+
+const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+      buttonElement.classList.add('popup__save_disabled');
+    } else {
+      buttonElement.classList.remove('popup__save_disabled');
+    }
+};
+
+
 const setEventListeners = (formElement) => {
     const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    const buttonElement = formElement.querySelector('.popup__save');
     inputList.forEach((inputElement) => {
         inputElement.addEventListener('input', () => {
             checkInputValidity(formElement, inputElement);
+            toggleButtonState(inputList, buttonElement);
         });
     });
 };
