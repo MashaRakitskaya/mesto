7459cdea3b28373573601a1aa508e6d9324db8fa
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2020-12-22 19:28:46 +0100

    PopupWithForm And UserInfo

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -1,3 +1,25 @@
+import { Popup } from './Popup.js';
 export class PopupWithForm extends Popup {
+    constructor(popupSelector) {
+        super(popupSelector);
 
+    }
+    _getInputValues() {
+        // profileTitle.textContent = nameField.value;
+        // profileParagraph.textContent = titleField.value;
+    }
+
+    setEventListeners(popupCloseButton) {
+    super(setEventListeners(popupCloseButton));
+    editForm.addEventListener('submit', event => {
+        event.preventDefault();
+        // profileTitle.textContent = nameField.value;
+        // profileParagraph.textContent = titleField.value;
+        // closePopup(popupEditProfile);
+    });
+    }
+
+    close() {
+        super.close();
+    }
 }
---
+++ scripts/UserInfo.js
@@ -0,0 +1,15 @@
+export class UserInfo {
+    constructor({nameSelector, occupationSelector}) {
+        this._nameSelector = nameSelector;
+        this._occupationSelector = occupationSelector;
+    }
+
+    getUserInfo() {
+
+    }
+
+    setUserInfo() {
+
+    }
+
+}
