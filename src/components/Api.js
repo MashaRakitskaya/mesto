02794e3273c4e796c46715379e188c05670144c9
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2021-01-15 20:05:35 +0100

    post card

--- src/components/Api.js
+++ src/components/Api.js
@@ -18,6 +18,7 @@ export class Api {
             return Promise.reject(`Ошибка ${response.status}`)
         })
     }
+
     getUserInformation() {
         return fetch('https://mesto.nomoreparties.co/v1/cohort-19/users/me ', {
             headers: {
@@ -31,6 +32,21 @@ export class Api {
 
             return Promise.reject(`Ошибка ${response.status}`)
         })  
+    }
+
+    addCard(data) {
+        return fetch('https://mesto.nomoreparties.co/v1/cohort-19/cards', {
+            method: 'POST',
+            headers: {
+                authorization: '369f7f82-3628-418a-9ccf-d1d1496569f6',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: data.name,
+                link: data.link
+            })
+        })
+        .then(result => result.ok ? result.json() : Promise.reject(`Ошибка ${result.status}`))
     }
 }
   
